cea143cf491b80962fff4999e897ecaaa8ed54ec
akshata-khomane <73632010+akshata-khomane@users.noreply.github.com>
2020-11-02 12:17:59 +0530

    Create quetion8.py

---
+++ quetion8.py
@@ -0,0 +1,9 @@
+required_number = 9920162317
+
+while True:
+    number = eval(input("Enter the number\n"))
+    if number == required_number:
+        print ("GOT IT")
+        break
+    else:
+        print ("Wrong number try again")
